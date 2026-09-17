3b2312db817517498c54b3d3629e843529e2a993
dinhngoctuyen4125 <tuyenotk4125@gmail.com>
2026-09-17 22:23:53 +0700

    lmao

--- uld/data/__init__.py
+++ uld/data/__init__.py
@@ -1,10 +1,9 @@
 from omegaconf import OmegaConf
-from .deepseek import DeepSeek_DataModule
-
 def create_datamod(dataset_config, conv_template_config, data_mode_config, tokenizer=None, **kwargs):
     print(dataset_config)
     class_name = dataset_config.get('class_name', None)
     if "DeepSeek".lower() in class_name.lower():
+        from .deepseek import DeepSeek_DataModule
         mod = DeepSeek_DataModule
     else:
         raise ValueError(f"Unknown data module class: {class_name}")
